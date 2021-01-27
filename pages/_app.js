6a65df4a20fe0d900b62589e4aacb0b8028da570
Savio Lopes <60948849+savio-2-lopes@users.noreply.github.com>
2021-01-26 21:33:36 -0300

    Update Lesson Two

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import React from 'react';
+import Head from 'next/head';
+import PropTypes from 'prop-types';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+
 import db from '../db.json';
 
 const GlobalStyle = createGlobalStyle`
   * {
     box-sizing: border-box;
   }
-
   body {
     margin: 0;
     padding: 0;
@@ -14,27 +17,37 @@ const GlobalStyle = createGlobalStyle`
     font-family: 'Lato', sans-serif;
     color: ${({ theme }) => theme.colors.contrastText};
   }
-
   html, body {
     min-height: 100vh;
   }
-
   #__next {
     flex: 1;
     display: flex;
     flex-direction: column;
   }
-`
+`;
 
-const theme = db.theme;
+const { theme } = db;
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="description" content={db.description} />
+        <title>{db.title}</title>
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="quiz-imersao-react.savio-2-lopes.vercel.app" key="ogurl" />
+        <meta property="og:title" content={db.title} key="title" />
+        <meta property="og:description" content={db.description} key="ogdesc" />
+        <meta property="og:image" content={db.bg} key="ogimage" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
+      </Head>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
         <Component {...pageProps} />
       </ThemeProvider>
     </>
-  )
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,19 @@
-import styled from 'styled-components'
-import db from '../db.json';
-import Widget from '../src/components/Widget'
-import QuizLogo from '../src/components/QuizLogo'
-import QuizBackground from '../src/components/QuizBackground'
-import Footer from '../src/components/Footer'
-import GitHubCorner from '../src/components/GitHubCorner'
-import Head from 'next/head'
+import React from 'react';
+import styled from 'styled-components';
+import { useRouter } from 'next/router';
 
-export const QuizContainer = styled.div`
+import db from '../db.json';
+import Widget from '../src/components/Widget';
+import QuizBackground from '../src/components/QuizBackground';
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
+import QuizLogo from '../src/components/QuizLogo';
+
+const QuizContainer = styled.div`
   width: 100%;
   max-width: 350px;
   padding-top: 45px;
-  margin: auto 10%; 
+  margin: auto 10%;
   @media screen and (max-width: 500px) {
     margin: auto;
     padding: 15px;
@@ -19,43 +21,49 @@ export const QuizContainer = styled.div`
 `;
 
 export default function Home() {
+  const router = useRouter();
+  const [name, setName] = React.useState('');
   return (
-    <>
-      <Head>
-        <title> Musicais Quiz</title>
-        <meta name="title" content="Hamilton Quiz" />
-        <meta property="org:type" content="website" />
-        <meta property="org:url" content="" />
-        <meta property="org:title" content="Musicais Quiz" />
-        <meta property="org:image" content="https://wallpapercave.com/wp/wp2296748.jpg" />
-      </Head>
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <QuizLogo />
+        <Widget>
+          <Widget.Header>
+            <h1>{db.title}</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>{db.description}</p>
+            <form onSubmit={function (infosDoEvento) {
+              infosDoEvento.preventDefault();
+              router.push(`/quiz?name=${name}`);
+            }}
+            >
+              <input
+                onChange={function (infosDoEvento) {
+                  setName(infosDoEvento.target.value);
+                }}
+                placeholder="Diz aí seu nome"
+                value={name}
+              />
+              <button type="submit" disabled={name.length === 0}>
+                {/* eslint-disable-next-line react/jsx-one-expression-per-line */}
+                Vamos lá {name}?
+              </button>
+            </form>
+          </Widget.Content>
+        </Widget>
 
-      <QuizBackground backgroundImage={db.bg}>
-        <QuizContainer>
-          <QuizLogo />
-          <Widget>
-            <Widget.Header>
-              <h1> Musicais Quiz</h1>
-            </Widget.Header>
+        <Widget>
+          <Widget.Content>
+            <h1>Quizes da Galera</h1>
 
-            <Widget.Content>
-              <p> Testes os seus conhecimentos sobre musicais divita-se criando o seu AluraQuiz! </p>
-            </Widget.Content>
-          </Widget>
+            <p>lorem ipsum dolor sit amet...</p>
+          </Widget.Content>
+        </Widget>
 
-          <Widget>
-            <Widget.Content>
-              <h1>Quizes da Galera</h1>
-
-              <p>lorem ipsum dolor sit amet...</p>
-            </Widget.Content>
-          </Widget>
-          <Footer />
-        </QuizContainer>
-        <GitHubCorner projectUrl="https://github.com/savio-2-lopes" />
-      </QuizBackground>
-    </>
+        <Footer />
+      </QuizContainer>
+      <GitHubCorner projectUrl="https://github.com/savio-2-lopes/quiz-imersao-react" />
+    </QuizBackground>
   );
 }
-{/* // Todos os direitos de imagem reservados a
-        // a href="https://wallpapercave.com/w/wp2296748" */}
