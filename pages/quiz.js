ea17ffb6c624c02d213512f6dfdd631fd9fc2c52
Savio Lopes <60948849+savio-2-lopes@users.noreply.github.com>
2021-01-27 23:07:53 -0300

    Modify lesson tree

--- pages/quiz.js
+++ pages/quiz.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import { useRouter} from 'next/router';
+import { useRouter} from 'next/router'; 
 import { useEffect, useState } from "react";
 
 import QuizBackground from '../src/components/QuizBackground';
