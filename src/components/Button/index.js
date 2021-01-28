1ab5e2497a39a789aaa37f827b0b86d748b46490
Savio Lopes <60948849+savio-2-lopes@users.noreply.github.com>
2021-01-27 23:03:09 -0300

    Update lesson three

---
+++ src/components/Button/index.js
@@ -0,0 +1,35 @@
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const Button = styled.button`
+  background-color: ${({ theme }) => theme.colors.primary};
+  color: ${({ theme }) => theme.colors.contrastText};
+  border-radius: ${({ theme }) => theme.borderRadius};
+  border: 0;
+  
+  width: 100%;
+  padding: 15px 15px;
+  font-weight: bold;
+  font-size: 14px;
+  line-height: 1;
+  text-transform: capitalize;
+  outline: 0;
+  transition: .3s;
+  cursor: pointer;
+  
+  &:hover,
+  &:focus {
+    opacity: .9;
+  }
+  &:disabled {
+    background-color: #a1c925;
+    cursor: not-allowed;
+  }
+`;
+
+Button.propTypes = {
+  type: PropTypes.oneOf(['submit', 'type', 'button']).isRequired,
+  children: PropTypes.node.isRequired,
+};
+
+export default Button;
--- src/components/Widget/index.js
+++ src/components/Widget/index.js
@@ -8,13 +8,13 @@ const Widget = styled.div`
   border-radius: 4px;
   overflow: hidden;
   h1, h2, h3 {
-    font-size: 20px;
+    font-size: 16px;
     font-weight: 700;
     line-height: 1;
     margin-bottom: 0;
   }
   p {
-    font-size: 16px;
+    font-size: 14px;
     font-weight: 400;
     line-height: 1;
   }
@@ -26,6 +26,7 @@ Widget.Header = styled.header`
   align-items: center;
   padding: 18px 32px;
   background-color: ${({ theme }) => theme.colors.primary};
+  
   * {
     margin: 0;
   }
@@ -33,41 +34,33 @@ Widget.Header = styled.header`
 
 Widget.Content = styled.div`
   padding: 24px 32px 32px 32px;
-  & > *:first-child{
+  & > *:first-child {
     margin-top: 0;
   }
-  & >*:last-child{
+  & > *:last-child {
     margin-bottom: 0;
   }
   ul {
     list-style: none;
     padding: 0;
   }
-  input {
-    font-family: 'Lato', sans-serif;
-    color: ${({ theme }) => theme.colors.contrastText};
-    width: 100%;
-    margin-bottom: 14px;
-    padding: 7px 15px;
-    border: 1px solid ${({ theme }) => theme.colors.primary};
-    background-color: ${({ theme }) => theme.colors.mainBg};
-    border-radius: 4px;
-    font-size: 16px;
-  }
-  button {
-    font-family: 'Lato', sans-serif;
-    color: ${({ theme }) => theme.colors.contrastText};
-    width: 100%;
-    padding: 10px 16px;
-    border: none;
-    box-shadow: 0px 0px 2px rgba(0, 0, 0, 0.12), 0px 2px 2px rgba(0, 0, 0, 0.24);
-    background-color: ${({ theme }) => theme.colors.secondary};
-    border-radius: 4px;
-    font-size: 16px;
-    font-weight: 700;
-  }
-  button:disabled {
-    background-color: ${({ theme }) => theme.colors.disabled};
+`;
+
+Widget.Topic = styled.a`
+  outline: 0;
+  text-decoration: none;
+  color: ${({ theme }) => theme.colors.contrastText};
+  background-color: ${({ theme }) => `${theme.colors.primary}`};
+  padding: 10px 15px;
+  margin-bottom: 8px;
+  cursor: pointer;
+  border-radius: ${({ theme }) => theme.borderRadius};
+  transition: .3s;
+  display: block;
+  
+  &:hover,
+  &:focus {
+    opacity: .9;
   }
 `;
 
